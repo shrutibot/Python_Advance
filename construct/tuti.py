54d94f2712fc0e761ce2a4bd16019f9dbecd38c2
shrutibot <shrutithapa010@gmail.com>
2025-12-19 10:04:13 +0545

    inital commit

---
+++ construct/tuti.py
@@ -0,0 +1,25 @@
+import string
+my_age=int(input("Enter your age:"))
+print (my_age)
+
+favfood= input("WHAT IS YOUR FAVORITE FOOD?")
+print (favfood)
+
+string_var="42"
+int_var=int(string_var)
+print(int_var )
+
+float=3.14159 
+string_var2=str(float)
+print(string_var2)
+
+a="hello"
+b="world"
+c=a + " " + b
+print(c)
+
+p="Python"
+print(p[2])
+
+sentence=input("Enter a sentence: ")
+print(sentence[:5])
